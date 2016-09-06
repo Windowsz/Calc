f2c1d307052812872b3ee67d710dbf30b004443b
Windows'z <Windows'z@WINDOWSZ-PC>
2016-09-07 00:50:36 +0700

    Add project files.

---
+++ Calc/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            string operand;
+            int answer;
+
+            // enter value to calculator
+            Console.WriteLine("Please Enter Number 1 : ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Operand ( + , - , * , / ) : ");
+            operand = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Number 2 : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            //operand + - * / 
+            switch (operand)
+            {
+                case "+":
+                    answer = a + b;
+                    break;
+                case "-":
+                    answer = a - b;
+                    break;
+                case "*":
+                    answer = a * b;
+                    break;
+                case "/":
+                    answer = a / b;
+                    break;
+                default:
+                    answer = 0;
+                    break;
+            }
+            //answer
+            Console.WriteLine(a.ToString() + " " + operand + " " + b.ToString() + " = " + answer.ToString());
+            Console.ReadLine();
+        }
+    }
+}
